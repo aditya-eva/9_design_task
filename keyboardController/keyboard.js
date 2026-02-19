2520ca7feb1ecd310ef55d0cd3c3c87dab1a33e6
Aditya Mohaty <adityamohaty@evawinoptimize.com>
2026-02-19 16:33:06 +0530

    refactor keyboard event handling for improved geometry updates

--- keyboardController/keyboard.js
+++ keyboardController/keyboard.js
@@ -15,33 +15,39 @@ export function setupKeyboard(scene) {
   }
 
   window.addEventListener("keydown", (e) => {
-    if (e.key === "2") {
-        removeEdgeLine(scene);
-        mesh.geometry = geometries.capsule();
-        mesh.material = materials.standard;
-    } else if (e.key === "3") {
-        mesh.geometry = geometries.cone();
-        mesh.material = materials.standard;
-    } else if (e.key === "4") {
-        mesh.geometry = geometries.cylinder();
-    } else if (e.key === "5") {
-        const edgeGeometry = geometries.cone();
-        const edges = new THREE.EdgesGeometry(edgeGeometry);
-        edgeLine = new THREE.LineSegments(edges);
-        scene.add(edgeLine);
-    } else if (e.key === "6") {
-        mesh.geometry = geometries.shapeArc();
-    } else if (e.key === "7") {
-        mesh.geometry = geometries.torus();
-    } else if (e.key === "8") {
-        mesh.geometry = geometries.tube();
-        mesh.material = materials.phong;
-    } else if (e.key === "9") {
-        mesh.geometry = geometries.randomBuffer();
-        mesh.material = materials.normal;
-    } else {
-        mesh.geometry = geometries.box();
-        mesh.material = materials.standard;
+    {
+        if(e.key !== "5") {
+            scene.add(mesh);
+            removeEdgeLine(scene);
+        }
+        if (e.key === "2") {
+            mesh.geometry = geometries.capsule();
+            mesh.material = materials.standard;
+        } else if (e.key === "3") {
+            mesh.geometry = geometries.cone();
+            mesh.material = materials.standard;
+        } else if (e.key === "4") {
+            mesh.geometry = geometries.cylinder();
+        } else if (e.key === "5") {
+            scene.remove(mesh);
+            const edgeGeometry = geometries.cone();
+            const edges = new THREE.EdgesGeometry(edgeGeometry);
+            edgeLine = new THREE.LineSegments(edges);
+            scene.add(edgeLine);
+        } else if (e.key === "6") {
+            mesh.geometry = geometries.shapeArc();
+        } else if (e.key === "7") {
+            mesh.geometry = geometries.torus();
+        } else if (e.key === "8") {
+            mesh.geometry = geometries.tube();
+            mesh.material = materials.phong;
+        } else if (e.key === "9") {
+            mesh.geometry = geometries.randomBuffer();
+            mesh.material = materials.normal;
+        } else {
+            mesh.geometry = geometries.box();
+            mesh.material = materials.standard;
+        }
     }
   });
 }
