c593c7009d997eea8df5dff673d8b34d04a0bec1
Aditya Mohaty <adityamohaty@evawinoptimize.com>
2026-02-24 11:28:55 +0530

    Added vite config

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        project1: resolve(__dirname, "project1.html"),
+        project2: resolve(__dirname, "project2.html"),
+      },
+    },
+  },
+});
