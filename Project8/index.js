b7e886f328fdda411f3c07b3300327130c7165e6
Aditya Mohaty <adityamohaty@evawinoptimize.com>
2026-03-18 12:12:11 +0530

    updated variable names

--- Project8/index.js
+++ Project8/index.js
@@ -7,14 +7,19 @@ import { groupAllFields } from "./sidebar/groupAllFields";
 import { FontLoader, OrbitControls } from "three/examples/jsm/Addons.js";
 import { positionFieldInputs, positionProfileInputs } from "./helper";
 import { createDimension } from "./dimensions/createDimension";
+import { createHandle2D } from "../Project9/createHandle2D";
+
 
 const date = document.getElementById("date");
 const currDate = new Date();
 const formattedDate = currDate.toLocaleDateString('en-GB');
 date.value = formattedDate;
 
+
 const scene = new THREE.Scene();
 scene.background = new THREE.Color("white");
+
+
 
 
 const camera = new THREE.PerspectiveCamera(
@@ -24,17 +29,23 @@ const camera = new THREE.PerspectiveCamera(
   10000
 );
 
-const width = 200;
-const height = 200;
-const outerH1 = 6;
-const beadHeight = 2;
+
+let params = JSON.parse(localStorage.getItem("windowParams"));
+let { side, handleSide, handlePosition, handleLength, handleWidth, ghhValue, windowLength, windowWidth, outerH1, beadHeight } = params
+
+
+const width = windowLength;
+const height = windowWidth;
+
 
 camera.position.z = Math.max(2 * width, 2 * height);
 
+
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement); 
+document.body.appendChild(renderer.domElement);
 const drawingBoard = new THREE.Group();
+
 
 const controls = new OrbitControls(camera, renderer.domElement);
 const geometry = new THREE.PlaneGeometry(4 * width, 2 * height);
@@ -44,13 +55,16 @@ const material = new THREE.MeshBasicMaterial({
 });
 // console.log(geometry.attributes.position.array)
 
+
 const size = 10000;
 const divisions = 2000;
 const gridHelper = new THREE.GridHelper(size, divisions, new THREE.Color("blue"), new THREE.Color("blue"));
 
+
 gridHelper.rotateX(Math.PI / 2);
 gridHelper.position.z = -1;
 // scene.add(gridHelper);
+
 
 const LineMat = new THREE.LineBasicMaterial({ color: "black" })
 const points = [
@@ -61,21 +75,66 @@ const points = [
   new THREE.Vector3(-2*width, -height, 0),
 ];
 
+
 const geo = new THREE.BufferGeometry().setFromPoints(points);
 const rect = new THREE.Line(geo, LineMat);
 
+
 drawingBoard.add(rect);
+
 
 const plane = new THREE.Mesh(geometry, material);
 drawingBoard.add(plane);
 
+
 const windowParts = createWindow(width, height, outerH1, beadHeight, scene);
+
 
 const nameSection = createYourNameSection(width, height, 3 * width / 4, 3 * height / 4);
 const groupedShapes = groupShapes(width, height);
 
+
 const groupFields = groupAllFields(width, 2 * height);
 drawingBoard.add(groupFields.group);
+
+
+console.log(localStorage.getItem("windowParams"))
+// side -> 2
+// handleSide -> inside/outside
+// handlePosition -> L/R/T/B
+
+if(handleSide === "inside") {
+  const handle = createHandle2D(handleLength, handleWidth, side, handlePosition);
+ 
+  if(handlePosition === "bottom") {
+    handle.rotation.z = Math.PI/2
+    handle.position.x = width/2 + handleLength/6;
+    handle.position.y = outerH1/2;
+    windowParts.bottomSection.add(handle)
+  }
+ 
+  if(handlePosition === "top") {
+    handle.rotation.z = Math.PI/2
+    handle.position.y -= outerH1/2
+    handle.position.x = width/2 + handleLength/6
+    windowParts.topSection.add(handle)
+  }
+
+
+  if(handlePosition === "left") {
+    handle.position.y = ghhValue - handleLength/6
+    handle.position.x = outerH1/2
+    windowParts.leftSection.add(handle)
+  }
+ 
+  if(handlePosition === "right") {
+    handle.position.y = ghhValue - handleLength/6
+    handle.position.x -= outerH1/2
+    windowParts.rightSection.add(handle)
+  }
+}
+
+
 drawingBoard.add(windowParts.topSection)
 drawingBoard.add(windowParts.leftSection)
 drawingBoard.add(windowParts.bottomSection)
@@ -89,20 +148,24 @@ drawingBoard.add(dashes.dashedLineVertical);
 drawingBoard.add(dashes.dashedLineHorizontal);
 scene.add(drawingBoard)
 
+
 window.addEventListener("resize", () => {
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
   renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
+
 const loader = new FontLoader();
 
+
 loader.load("/fonts/helvetiker_regular.typeface.json", (font)=>{
+
 
     const dimension = createDimension(
         -width/2,      
         width/2,    
-        -(height/2 + height/10),     
+        -(height/2 + height/10),    
         `${width}`,  
         font,
         true
@@ -110,16 +173,19 @@ loader.load("/fonts/helvetiker_regular.typeface.json", (font)=>{
     const dimension2 = createDimension(
         -height/2,      
         height/2,    
-        (width/2 + width/10),     
+        (width/2 + width/10),    
         `${height}`,  
         font,
         false
     );
 
+
     scene.add(dimension);
     scene.add(dimension2);
 
+
 });
+
 
 function animate() {
   requestAnimationFrame(animate);
@@ -142,6 +208,8 @@ function animate() {
   );
   controls.update();
 }
+
+
 
 
 animate();
